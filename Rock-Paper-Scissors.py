4b91573d2535f4bc1257892bc5fe2bf8a87a6b5f
Edward Bristow <SirEdBr@users.noreply.github.com>
2015-04-01 14:00:26 +0100

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,1 +1,13 @@
 print("Welcome to 'Rock, Paper, Scissors' !")
+print("Press Ctrl and C to stop this program")
+while True:
+    choice=input("What do you want to choose? Type 1 for rock, 2 for paper and 3 for scissors: ")
+    if choice == 1:
+        print("")
+    elif choice == 2:
+        print("")
+    elif choice == 3:
+        print("")
+    else:
+        print("That's not a choice! Try again.")
+        print("")
