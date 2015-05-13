c47308d60455cdcefca765b56292621048b46820
Edward Bristow <SirEdBr@users.noreply.github.com>
2015-05-13 13:45:05 +0100

    Finish - Hopefully no bugs!

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -8,6 +8,29 @@ def find_choice(a):
     elif a == 3:
         return 'scissors!'
 
+def who_won(cc, choice):
+    if choice == '1':
+        if cc == 'rock!':
+            return "It's a tie!"
+        elif cc == 'paper!':
+            return 'You lost!'
+        elif cc == 'scissors!':
+            return 'You won!'
+    elif choice == '2':
+        if cc == 'paper!':
+            return "It's a tie!"
+        elif cc == 'scissors!':
+            return 'You lost!'
+        elif cc == 'rock!':
+            return 'You won!'
+    elif choice == '3':
+        if cc == 'scissors!':
+            return "It's a tie!"
+        elif cc == 'rock!':
+            return 'You lost!'
+        elif cc == 'paper!':
+            return 'You won!'
+
 print("Welcome to 'Rock, Paper, Scissors' !")
 print("Press Ctrl and C to stop this program")
 while True:
@@ -17,6 +40,7 @@ while True:
         print("You have chosen 'rock' !")
         print("The computer chooses...")
         print(cc)
+        print (who_won(cc, choice))
     elif choice == '2':
         cc = find_choice(random.randint(1,3))
         print("You have chosen 'paper' !")
@@ -27,6 +51,7 @@ while True:
         print("You have chosen 'scissors' !")
         print("The computer chooses...")
         print(cc)
+        print (who_won(cc, choice))
     else:
         print("That's not a choice! Try again.")
         pass
