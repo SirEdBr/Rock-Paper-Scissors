e73439ed17bc1cd0eca31c66bf9e25729bf1000e
Edward Bristow <SirEdBr@users.noreply.github.com>
2015-05-21 13:49:14 +0100

    BLAH

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,5 +1,7 @@
 import random
 
+def stats():
+
 def find_choice(a):
     if a == 1:
         return 'rock!'
@@ -12,30 +14,39 @@ def who_won(cc, choice):
     if choice == '1':
         if cc == 'rock!':
             return "It's a tie!"
+            results = 'tie'
         elif cc == 'paper!':
             return 'You lost!'
+            results = 'lost'
         elif cc == 'scissors!':
             return 'You won!'
+            results = 'won'
     elif choice == '2':
         if cc == 'paper!':
             return "It's a tie!"
+            results = 'tie'
         elif cc == 'scissors!':
             return 'You lost!'
+            results = 'lost'
         elif cc == 'rock!':
             return 'You won!'
+            results = 'won'
     elif choice == '3':
         if cc == 'scissors!':
             return "It's a tie!"
+            results = 'tie'
         elif cc == 'rock!':
             return 'You lost!'
+            results = 'lost'
         elif cc == 'paper!':
             return 'You won!'
+            results = 'won'
 
 print("Welcome to 'Rock, Paper, Scissors' !")
 print("Press Ctrl and C to stop this program")
 while True:
     print('')
-    choice=input("What do you want to choose? Type 1 for rock, 2 for paper and 3 for scissors: ")
+    choice=input("What do you want to choose? Type 1 for rock, 2 for paper and 3 for scissors. Alternatively, you can ask how many times you have won, lost and drawn by typing stats: ")
     if choice == '1':
         cc = find_choice(random.randint(1,3))
         print("You have chosen 'rock' !")
@@ -54,7 +65,8 @@ while True:
         print("The computer chooses...")
         print(cc)
         print(who_won(cc, choice))
+    elif choice == 'stats':
+        print(stats)
     else:
         print("That's not a choice! Try again.")
         pass
-    
